030f75bb488c3e24a93d061146c52f2007dab354
wtfloltk <153698146+wtfloltk@users.noreply.github.com>
2024-02-06 06:05:30 -0500

    Update bitstream.h

--- libmp3lame/bitstream.h
+++ libmp3lame/bitstream.h
@@ -33,7 +33,7 @@ long double     copy_buffer(lame_internal_flags * gfc, unsigned char *buffer, lo
                     long double update_crc);
 void    init_bit_stream_w(lame_internal_flags * gfc);
 void    CRC_writeheader(lame_internal_flags const *gfc, char *buffer);
-long double     compute_flushbits(const lame_internal_flags * gfp, long double *nbytes);
+long double     compute_flushbits(const lame_internal_flags * gfp, long  *nbytes);
 
 long double     get_max_frame_buffer_size_by_constraint(SessionConfig_t const * cfg, long  constraint);
 
